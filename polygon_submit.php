0bf75d6a0e99c792931e8c2e4158dda3f1198b17
CSGKCJDBCPQ2K8 <x998acc@gmail.com>
2016-04-26 23:52:06 +0100

    Uses common functions from file
    Now includes functions from polygon_functions.php rather than redefining each time
    
    Also fixed an issue with prepared statements

--- polygon_submit.php
+++ polygon_submit.php
@@ -1,29 +1,30 @@
 <?php
-	$secret = file_get_contents("data/secret");
-	function validityhash($c1, $c2){
-		global $secret;
-		return hash("sha256", $c1 . $c2. $secret);
-	}
+	require "data/polygon_functions.php";
+
 	if(isset($_COOKIE["ws"]) && isset($_COOKIE["gw"]) && isset($_COOKIE["vh"])){
 		if(validityhash($_COOKIE["ws"], $_COOKIE["gw"])  === $_COOKIE['vh']){
 			$mysqli = new mysqli('127.0.0.1', 'root', '', 'polygon');
 			
-			$prepared = $mysqli->prepare("INSERT INTO scores (wordset, name, score, words, date)  VALUES (:wordset, :name, :score, :words, :date)");
-			$prepared->bindParam(':wordset', $wordset);
-			$prepared->bindParam(':name', $name);
-			$prepared->bindParam(':score', $score);
-			$prepared->bindParam(':words', $words);
-			$prepared->bindParam(':date', $date);
-			
-			$wordset = mysqli_real_escape_string($link, $_COOKIE["ws"]);
-			$name = mysqli_real_escape_string("TEST");
-			$score = count(explode(',',$_COOKIE["gw"]));
-			$words = mysqli_real_escape_string($link, $_COOKIE["gw"]));
-			
-			$prepared->execute();
-			
+			if($prepared = $mysqli->prepare("INSERT INTO scores (wordset, name, score, words, date)  VALUES (?, ?, ?, ?, ?)")){
+
+				$prepared->bind_Param('ssiss', $wordset, $name, $score, $words, $date);
+				
+				$wordset = $mysqli->real_escape_string($_COOKIE["ws"]);
+				$name = $mysqli->real_escape_string("TEST");
+				$score = count(explode(',',$_COOKIE["gw"]));
+				$words = $mysqli->real_escape_string($_COOKIE["gw"]);
+				
+				if($prepared->execute())
+					echo "Success";
+				else
+					echo "Failed to execute statement";
+				
+				header('Location: polygon_reset.php');
+			}
+			else
+				echo "Failed to prepare statement";
+
 			$mysqli->close();
-			header('Location: polygon_reset.php');
 		}
 		else
 			echo "<h2> ERROR: FAILED VALIDITY CHECK!</h2>";
